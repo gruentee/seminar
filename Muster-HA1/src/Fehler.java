9425061ce7ec2776c2828d9839f4bca8eba61e84
kritzelbit <@Ohrensessel>
2014-10-30 12:19:36 +0100

    Musterlösung HA 1

---
+++ Muster-HA1/src/Fehler.java
@@ -0,0 +1,24 @@
+
+class Fehler {
+
+	Integer variable1;
+	
+	Integer variable2;
+	
+	String variable3;
+	
+	Boolean variable4;
+	
+	void methode1() {
+		System.out.println("Methode 1 aufgerufen");
+	}
+	
+	void methode2() {
+		System.out.println("Methode 2 aufgerufen");
+	}
+	
+	void methode3() {
+		System.out.println("Methode 3 aufgerufen");
+	}
+	
+}
