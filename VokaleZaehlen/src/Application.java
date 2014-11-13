9a0ea3403af5f50c4c77e0cfc949559d44b78162
kritzelbit <@Ohrensessel>
2014-11-13 09:43:26 +0100

    Musterlösung zu HA-3 (Vokale zählen) hinzugefügt.

---
+++ VokaleZaehlen/src/Application.java
@@ -0,0 +1,15 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		VokalZaehler zaehler = new VokalZaehler();
+		
+		zaehler.zaehleVokale("hallo!");
+		zaehler.zaehleVokale("In diesem Satz gibt es nicht besonders viele a's.");
+		zaehler.zaehleVokale("aabaabaaaaaaabbbaeiou");
+		zaehler.zaehleVokale("AABAABAAAAAAABBBAEIOU");
+		
+	}
+
+}
---
+++ VokaleZaehlen/src/VokalZaehler.java
@@ -0,0 +1,31 @@
+
+public class VokalZaehler {
+	
+	
+	public Integer zaehleVokale(String zeichenKette) {
+		zeichenKette = zeichenKette.toLowerCase();
+		Integer aktuellePosition = 0;
+		Integer letztePosition = zeichenKette.length()-1;
+		Integer anzahlVokale = 0;
+		
+		while(aktuellePosition <= letztePosition) {
+			Character buchstabe = zeichenKette.charAt(aktuellePosition);
+			if(buchstabe == 'a') {
+				anzahlVokale++;
+			} else if (buchstabe == 'e') {
+				anzahlVokale++;
+			} else if (buchstabe == 'i') {	// Natürlich ist diese Lösung
+				anzahlVokale++;				// sehr unschön - da wir aber
+			} else if (buchstabe == 'o') {	// zu diesem Zeitpunkt noch
+				anzahlVokale++;				// keine Schleifen benutzt haben,
+			} else if (buchstabe == 'u') { 	// ist die eine der möglichen
+				anzahlVokale++;				// "Notlösungen".
+			}
+			aktuellePosition++;
+		}
+		
+		System.out.println("Anzahl aller Vokale in '" + zeichenKette + "': " + anzahlVokale);
+		return 0;
+	}
+
+}
